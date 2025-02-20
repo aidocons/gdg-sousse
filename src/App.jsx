953f7c36268fbd1e23da534a8d8dcb40374cc3e4
aidocons <consultant-butlyai@aidodev.com>
2025-02-20 21:05:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hello From GDG Sousse!</h1>;
+}
+
+export default App;
---
+++ src/components/TodoCard/TodoCard.jsx
@@ -0,0 +1,9 @@
+import styles from "./TodoCard.module.css"
+
+function TodoCard() {
+  return <div className={styles.box}>
+    <p>This is a card</p>
+  </div>
+}
+
+export default TodoCard
